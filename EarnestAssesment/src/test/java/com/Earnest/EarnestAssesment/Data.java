b65f83e0c830efb1604d434b7ef5dba1fe58c4b2
pratimachinthala <37169723+pratimachinthala@users.noreply.github.com>
2018-05-18 14:49:20 -0700

    Add files via upload

--- EarnestAssesment/src/test/java/com/Earnest/EarnestAssesment/Data.java
+++ EarnestAssesment/src/test/java/com/Earnest/EarnestAssesment/Data.java
@@ -1,16 +1,16 @@
 package com.Earnest.EarnestAssesment;
 
 public class Data {
-	 public static int Lion = 3;
-	   public static int Elephant = 1;
-	   public static int Zebra = 5;
-	   public static int Giraffe = 8;
+	 public static int Lion = 4;
+	   public static int Elephant = 2;
+	   public static int Zebra = 1;
+	   public static int Giraffe = 5;
 	   public static String State = "CA";
 
-	   public static int InvalidLion = 30;
-	   public static int InvalidZebra = 50;
-	   public static int InvaldGiraffe = 40;
+	   public static int InvalidLion = 50;
+	   public static int InvalidZebra = 70;
+	   public static int InvaldGiraffe = 60;
 	   public static int InvalidElephant = 48;
-	   public static String InvalidState = "NN";
+	   public static String InvalidState = "WW";
 
 }
